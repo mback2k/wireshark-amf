dcc84789072ca6231cd69143d8124f9ddeca9f6a
Marc Hoersken <info@marc-hoersken.de>
2013-10-27 11:44:12 +0100

    Added copyright information

--- packet-amf.c
+++ packet-amf.c
@@ -1,5 +1,7 @@
 /* packet-amf.c
  *
+ * Copyright (C) 2012 - 2013, Marc Hoersken, <info@marc-hoersken.de>
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
